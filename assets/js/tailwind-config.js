186d4cdd9b9180a1833068e7e968aca85617c387
Pham Tuan Minh <tuanminhxd1@gmail.com>
2026-07-27 13:16:13 +0700

    cấu trúc lại code và file

---
+++ assets/js/tailwind-config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                dark: '#0B0F19',
+                darker: '#05080F',
+                primary: '#6366f1', // Indigo
+                secondary: '#a855f7', // Purple
+                accent: '#14b8a6', // Teal
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            }
+        }
+    }
+}
